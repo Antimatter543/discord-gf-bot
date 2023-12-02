e416c2fe71d36e608646ed5ecd4e85317670e64e
Anti Matter <spiralknightsowner@gmail.com>
2023-12-02 22:12:03 +1000

    Refactored code to be in a class. works for multiple people at once now, they all have their own distinct histories that is sent to the api

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+This bot is effectively set up to be incredibly supportive, wants you to succeed, always rooting for you.
+
+
+
+It actually functions quite well as anti-incel material tbh. It wants you to win, but it enforces boundaries and wants you to be a good human.
+
+## Potential TODOS
+- have a local LLM so it doesn't fucking cost my ass
+- save and store histories into files (json?) so it can pull from past as required.
+
 ### Setup
 - Make a config.py file in /config with a `TOKEN = {YOUR TOKEN HERE}` variable.
--- main.py
+++ main.py
@@ -1,54 +1,63 @@
+from typing import Any
 import discord
-from config.config import TOKEN, OPENAI_TOKEN, SYSTEM_PROMPT
+from discord.flags import Intents
+from config.config import DISCORD_TOKEN, OPENAI_TOKEN, SYSTEM_PROMPT
 from openai import OpenAI
 
-messages = []
-async def response(discord_message: discord.Message):
-    messages = [{"role": "system", "content": SYSTEM_PROMPT}]
-    messages.append({"role": "user", "content": discord_message.content})
-    chat_completion = openai_client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages= messages,
+
+
+
+
+# TODO : Refactor the shit above to be below. see gpt
+class DiscordPartner(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.messages = {} # Could convert to a dictionary which maps user id -> message history
+
+        self.openai_client = OpenAI(api_key=OPENAI_TOKEN)
+
         
-        max_tokens= 128,
-    )
-    print("Message loaded")
-     ### For chat completion and shit, we should just have it maintain the system prompt of being a gf, and the last 5 or so messages to save on token cost?
-    response_message : str = chat_completion.choices[0].message.content
+    async def on_ready(self):
+        print(f'We have logged in as {self.user}')
 
-    messages.append({"role": "assistant", "content": response_message})
-    print(f"messages here {messages}")
-    await discord_message.author.send(response_message)
+    async def on_message(self, message : discord.Message):
+        print(f'Message from {message.author}: {message.content}')
+        
+        if message.author == self.user: # ignore messages from ourselves (the bot)
+            return
+        
+        if message.content.startswith('helloa'):
+            # print(message.author)
+            await message.channel.send('Hello aaaaa!')
+        elif message.content.startswith('$'):
+            await self.response(message)
 
-## OpenAI
-openai_client = OpenAI(api_key=OPENAI_TOKEN)
+    async def response(self, discord_message: discord.Message):
+        if self.messages.get(discord_message.author) == None:
+            self.messages[discord_message.author] = [{"role": "system", "content": SYSTEM_PROMPT}] 
+        self.messages[discord_message.author].append({"role": "user", "content": discord_message.content})
 
-messages = []  # History of conversation, should be a list of shit like 
+        chat_completion = self.openai_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages= self.messages[discord_message.author],
+            
+            max_tokens= 256,
+        )
+        print("Message loaded")
+        ### For chat completion and shit, we should just have it maintain the system prompt of being a gf, and the last 5 or so messages to save on token cost?
+        response_message : str = chat_completion.choices[0].message.content
 
-### Discord
+        self.messages[discord_message.author].append({"role": "assistant", "content": response_message})
+        # print(f"messages here {self.messages}")
+
+        print(f"So {discord_message.author} sent: {discord_message.content}\n and received: {response_message}\n")
+        
+        await discord_message.author.send(response_message)
+        # await discord_message.author.send(response_message)
+# Initialize and run the bot
 intents = discord.Intents.default()
 intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message : discord.Message):
-    print(f'Message from {message.author}: {message.content}')
-    
-    if message.author == client.user: # ignore messages from ourselves (the bot)
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith("hii"):
-        await response(message)
-
-client.run(TOKEN)
+bot = DiscordPartner(intents=intents)
+bot.run(DISCORD_TOKEN)
 
 
-    
